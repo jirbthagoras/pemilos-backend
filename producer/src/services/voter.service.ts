d071fa7ca59c4569ac911fd79ce01a8dc670d56e
jirbthagoras <hansjabriel@gmail.com>
2025-06-21 15:58:34 +0700

    [~]feat/PushCount: finished the feat, already tested

--- producer/src/services/voter.service.ts
+++ producer/src/services/voter.service.ts
@@ -6,6 +6,7 @@ import { PostInsertVote } from "../dtos/vote.dto";
 import { Vote } from "../models/vote.model";
 import { getRedisClient } from "../configs/redis.config";
 import debounce from "lodash/debounce";
+import { getPusherClient } from "../configs/pusher.config";
 
 // receive one or more user, and then input it to database.
 export const voterSaveMany = async (
@@ -77,6 +78,9 @@ export const voterSaveVote = async (req: PostInsertVote, userId: string) => {
                     isVoted: true
                }
           })
+
+          // push livecount
+          voterPushLiveCount()
      } catch (err) {
           throw err
      } finally {
@@ -85,15 +89,64 @@ export const voterSaveVote = async (req: PostInsertVote, userId: string) => {
      }
 }
 
-export const voterGetVoteResult = async () => {
-     try {
-          const osis = await Vote.find({"label": "osis"})
-     } catch (err) {
-          throw err
-     }
+export const voterGetResult = async (label: string) => {
+  try {
+    const results = await Vote.aggregate([
+     // Dynamically search, this aggregate simply take all votes based on label
+      { $match: { label } },
+
+     // Groups the newly queried result by their candidateId, so the result is as much as the candidates available.
+     // Also adds a path or field named column, to sum up the grouped vote.
+      {
+        $group: {
+          _id: "$candidate",
+          count: { $sum: 1 },
+        },
+      },
+
+     //  Since the available identifier for candidates is only the _id. This $lookup will cast it
+     //  into a real candidates, with their own name, number, etc.
+      {
+        $lookup: {
+          from: "candidates",
+          localField: "_id",
+          foreignField: "_id",
+          as: "candidate",
+        },
+      },
+
+     //  candidate that looked up is an array, so let's cast it into one single beautiful object cihuy
+      { $unwind: "$candidate" },
+
+     //  Finalize the result, casts some unnecessary field like _id (set it to 0) and candidates (just do ont mention it)
+      {
+        $project: {
+          _id: 0,
+          name: "$candidate.name",
+          number: "$candidate.number",
+          count: 1,
+        },
+      },
+
+     //  sort based on number
+      { $sort: { number: 1 } }
+    ])
+
+    return results
+  } catch (err) {
+    throw err
+  }
 }
 
 // Will be implemented later. When the pusher is ready.
 export const voterPushLiveCount = debounce(async () => {
-     const result = await
-})
+     const result = {
+          osis: await voterGetResult("osis"),
+          mpk: await voterGetResult("mpk"),
+     }
+
+     const pusher = await getPusherClient()
+
+     pusher.trigger("pemilose", "pemilolot", result)
+     return
+}, 3000)
